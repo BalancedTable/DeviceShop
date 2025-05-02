c112a8a6193b1a152450eff7c126a0aad8a98ee2
Dmitriy <dimkam2004@gmail.com>
2025-05-02 18:59:04 +0300

    made the details for the products

---
+++ app/src/main/java/com/example/yeepee/ElementActivity.kt
@@ -0,0 +1,28 @@
+package com.example.yeepee
+
+import android.annotation.SuppressLint
+import android.os.Bundle
+import android.widget.TextView
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class ElementActivity : AppCompatActivity() {
+    @SuppressLint("MissingInflatedId")
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_element)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+        val title: TextView = findViewById(R.id.itemListTitle1)
+        val descr: TextView = findViewById(R.id.itemListDescription1)
+
+        title.text = intent.getStringExtra("itemTitle")
+        descr.text = intent.getStringExtra("itemText")
+    }
+}
